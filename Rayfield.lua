17ead6d19bdb5483d97ed7769df16502db6c3b8d
testingscriptutorial <itshxnglol@gmail.com>
2025-07-29 23:39:16 +0700

    Update Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -1,10 +1,13 @@
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Window = Rayfield:CreateWindow({
+
+Rayfield:LoadConfiguration()
+
+local MainWindow = Rayfield:CreateWindow({
    Name = "Rayfield",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
+   LoadingTitle = "Loading . . .",
    LoadingSubtitle = "by Sirius",
    ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
    Theme = "Serenity", -- Check https://docs.sirius.menu/rayfield/configuration/themes
@@ -26,24 +29,25 @@ local Window = Rayfield:CreateWindow({
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
 
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Untitled",
+      Title = "Tota hub | Key system",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      Note = "Key:1234", -- Use this to tell the user how to get a key
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"1234"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
-local Tab = Window:CreateTab("Main Script")
-local Section = Tab:CreateSection("Scripts")
+
+local MainTab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+
 
 local Button = Tab:CreateButton({
-   Name = "Universal Vspeed Changer",
-   Callback = function(loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/a570696f44faad573d3173ad68b5dd5e"))())
-   -- The function that takes place when the button is pressed
+   Name = "Universal Vspeed changer",
+   Callback = function()
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/a570696f44faad573d3173ad68b5dd5e"))()
    end,
 })
